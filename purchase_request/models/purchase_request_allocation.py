835a80c3fe1caf447d98c80ad2b0779287fc9555
fedimad <36986328+fedimad@users.noreply.github.com>
2024-10-15 08:55:35 +0100

    Update purchase_request_allocation.py
    add body_is_html=True as a parameter to message_post() methode in order to get a correct display of the message

--- purchase_request/models/purchase_request_allocation.py
+++ purchase_request/models/purchase_request_allocation.py
@@ -127,6 +127,6 @@ class PurchaseRequestAllocation(models.Model):
             po_line = allocation.purchase_line_id
             message_data = self._prepare_message_data(po_line, request, allocated_qty)
             message = self._purchase_request_confirm_done_message_content(message_data)
-            request.message_post(
+            request.message_post(body_is_html=True,
                 body=message, subtype_id=self.env.ref("mail.mt_comment").id
             )
